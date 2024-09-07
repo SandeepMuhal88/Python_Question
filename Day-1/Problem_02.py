aea5e0563f01edcca18b695951a22404d3bc8830
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-07 21:18:46 +0530

    Solveing problem

---
+++ Day-1/Problem_02.py
@@ -0,0 +1,16 @@
+# Q. write a program to print name
+# Example
+# 1. R a m
+name=input("Enter your name:-")
+for i in name:
+    print(i,end=" ")
+
+
+# 2. R
+#    a
+#    m
+
+
+# name=input("Enter your name:-")
+# for i in name:
+#     print(i)
