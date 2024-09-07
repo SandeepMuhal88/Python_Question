bd795820210b1bdcc6a6708fe32fcc4b53155a27
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-07 21:12:20 +0530

    Solveing problem 01

---
+++ Day-1/Problem_01.py
@@ -0,0 +1,14 @@
+# Q. Program to add to numbers
+
+# MEthod first
+# x=int(input("Enter first number:- "))
+# y=int(input("Enter ssecond number:- "))
+# print("Sum of two Numbers:-",x+y)
+
+# Method second
+# x,y=map(int, input("Enter two numbers:- ").split())
+# print("Sum of two numbers:-",x+y)
+
+# MEthod third
+x,y=map(int, input("Enter two numbers:- ").split(","))
+print("Sum of two numbers:-",x+y)
