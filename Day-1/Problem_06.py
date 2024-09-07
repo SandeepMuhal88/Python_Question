b98d11b3af2d2a4456c73c6b95ae1a3d616bc653
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-07 22:31:01 +0530

    Solving python problem

---
+++ Day-1/Problem_06.py
@@ -0,0 +1,18 @@
+# Q convert list to string
+# MEthod first
+# l1=["Sandeep",52,"Muhal",12.5]
+# print("First list:-",l1)
+# for i in l1:
+#     print(i,end=" ")
+
+#  Second method:
+# l1 = ["r", "a", "m"]
+# l2 = "".join(l1)
+# print(l2)
+# print(type(l2))
+
+
+l1=["S","d","e","a"]
+l2="".join(l1)
+print("String:-",l2)
+
