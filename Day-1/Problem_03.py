7ed12d9fa28bc9a40f0624a20f1b5be2d444d317
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-07 22:25:50 +0530

    Solving problem

---
+++ Day-1/Problem_03.py
@@ -0,0 +1,6 @@
+# Q. If input is "Rama krishna" then output is first and last charcter
+
+name=input("Enter your name:-")
+newname=name[0]+name[-1]
+print("The first and last character:-",newname)
+# In python have positive and negative indexing
