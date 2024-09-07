5ab420092ebb9dc69a37832067d2560f70c30149
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-07 22:28:46 +0530

    Solving python problem

---
+++ Day-1/Problem_04.py
@@ -0,0 +1,11 @@
+# Q convert string to reverse order
+# example
+# 1. jp -> pj
+# 2. ram -> mar
+# Using slicing
+name=input("Enter your name:- ")
+# Silcing have a Slicing this formate : Start:Stop:Step
+# print(name[2:])
+# print(name[:2])
+# print(name[::2])
+print(name[::-1])
