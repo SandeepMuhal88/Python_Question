83be34507128a9c0c39e71aaaa0efe1c40daa81d
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-17 20:16:32 +0530

    Solve problem

--- Day-4/Problem-018.py
+++ Day-4/Problem-018.py
@@ -1,1 +1,17 @@
 # Q18. what will happen when two class have same function and they inherits their properties to other class ?
+class ADD:
+    def Addition(self,Name):
+        self.x=Name
+        print(f"Add th number:-{self.x+Name}")
+
+class SUB:
+    def Addition(self,Name):
+        self.x=Name
+        print(f"Add SUB class number:-{self.x+Name}")
+
+
+class Other(ADD,SUB):
+    pass
+
+obj=Other()
+obj.Addition("Hetua")
