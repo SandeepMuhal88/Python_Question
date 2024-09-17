3845535908ea73b57af29ab4899f7a530329788f
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-17 20:12:29 +0530

    Solve problem

--- Day-4/Problem-015.py
+++ Day-4/Problem-015.py
@@ -1,1 +1,22 @@
 # Q15. explain multiple inheritance ?
+class Additation:
+    def add(self,x,y):
+        print(f"Sum of two numbers:- {x+y}")
+
+class Substraction:
+    def sub(self,x,y):
+        print(f"Subtract two number= {x-y}")
+
+class Multiply(Substraction,Additation):
+    def mul(self,x,y):
+        print(f"Multiply two number:-{x*y}")
+
+
+obj=Multiply()
+a=int(input("Enter first value:-"))
+b=int(input("Enter second value:-"))
+obj.mul(a,b)
+obj.sub(a,b)
+obj.add(a,b)
+
+
