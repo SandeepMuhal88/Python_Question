3701fdbcb320f7c355f9f36dc06b61a3499a895b
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-17 20:15:50 +0530

    Solve proble

--- Day-4/Problem-017.py
+++ Day-4/Problem-017.py
@@ -1,1 +1,80 @@
 # Q17. what is "method resoultion order(MRO)" ?
+
+# concept 1 (bases)
+
+
+# class A:
+#     pass
+
+
+# class B:
+#     pass
+
+
+# class C(A, B):
+#     pass
+
+
+# obj = B()
+# print(C.__bases__)
+
+class Person:
+    def __init__(Self):
+        print("I am from Person class:--))")
+        super().__init__()
+
+
+class Human(Person):
+    def __init__(self):
+       print("I am from Human class")
+       super().__init__()
+
+
+
+class Man(Person):
+
+    def __init__(self):
+       print("I am from Man class")
+       super().__init__()
+   
+
+
+
+class God(Human,Man):
+    def __init__(self):
+        print("I am from God class:))")
+        super().__init__()
+
+
+
+obj=God()
+# print(God.mro())
+# print(Man.__base__)
+
+
+# class A:
+#     def __init__(self):
+#         print("class A")
+#         super().__init__()
+
+
+# class B(A):
+#     def __init__(self):
+#         print("class B")
+#         super().__init__()
+
+
+# class C(A):
+#     def __init__(self):
+#         print("class C")
+#         super().__init__()
+
+
+# class D(B, C):
+
+#     def __init__(self):
+#         print("class D")
+#         super().__init__()
+
+
+# obj = D()
